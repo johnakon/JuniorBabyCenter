dc9a286ba65a3f321048302f1d603408e0aeeb33
johnakon <john.akonya@gmail.com>
2020-10-19 11:54:15 +0300

    changes made

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const path = require("path");
-const mongodb = require("mongodb");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongodb = require('mongodb');
 // const bcryptjs = require("bcryptjs");
-
+const path = require('path');
 
 const app = express();
 
 // Mongoose connects the app to the mongoDB.
-mongoose.connect("mongodb://localhost:27017/Baby-sitters",() => {
-  console.log("connected successfully to databse")
+mongoose.connect('mongodb://localhost:27017/Baby-sitters', () => {
+  console.log('connected successfully to databse');
 });
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 
@@ -24,41 +24,42 @@ const registrationRoutes = require('./routes/parentsRegistration');
 app.use('/register', registrationRoutes);
 
 app.get('/', (req, res, next) => {
-  res.render('index', {title : 'Juior Baby Center'})
-})
+  res.render('index', { title: 'Juior Baby Center' });
+});
 app.get('/index', (req, res, next) => {
-  res.render('index', {title : 'Juior Baby Center'})
-})
+  res.render('index', { title: 'Juior Baby Center' });
+});
 
 app.get('/About', (req, res, next) => {
-  res.render('about', {title : 'About us'})
-})
+  res.render('about', { title: 'About us' });
+});
 
 app.get('/Parents', (req, res, next) => {
-  res.render('parents', {title : 'Parents'})
-})
+  res.render('parents', { title: 'Parents' });
+});
 
 app.get('/Babysitter', (req, res, next) => {
-  res.render('babysitter', {title : 'Baby Sitters'})
-})
+  res.render('babysitter', { title: 'Baby Sitters' });
+});
 
 app.get('/Register', (req, res, next) => {
-  res.render('register', {title : 'Register'})
-})
-
+  res.render('register', { title: 'Register' });
+});
 
 app.get('/Appointment', (req, res, next) => {
-  res.render('appointment', {title : 'Appointment'})
-})
+  res.render('appointment', { title: 'Appointment' });
+});
 
+//serve static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
 
-
-
-
-
-
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  );
+}
 
 //Setting the server and to Listen to port 3000
 app.listen(5000, () => {
-    console.log("listening on 5000");
-  });
+  console.log('listening on 5000');
+});
